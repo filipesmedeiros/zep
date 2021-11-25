bf7d0d983f9bc349cae49cfe6411e8b2f1e3764a
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2021-11-25 10:26:26 +0000

    fix: lol parens

--- lib/preferences/biometricsAuth.ts
+++ lib/preferences/biometricsAuth.ts
@@ -3,7 +3,7 @@ import isiOS from '../isiOS'
 export const localStorageKey = 'prefers-biometrics'
 
 export const prefersBiometricsAuth = () =>
-  (localStorage.getItem(localStorageKey) ?? isiOS() ? 'false' : 'true') ===
+  (localStorage.getItem(localStorageKey) ?? (isiOS() ? 'false' : 'true')) ===
   'true'
 
 export const togglePrefersBiometricsAuth = () => {
