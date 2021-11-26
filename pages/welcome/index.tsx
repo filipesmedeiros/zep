155cf61c0da16f14187ed448b0ecf0a0b35404ed
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2021-11-26 10:17:12 +0000

    fix: colors

--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -22,11 +22,11 @@ const Welcome: NextPage = () => {
         and will only be decrypted for a few moments to send nano
       </aside>
       <div className="flex flex-col gap-3 justify-center w-full mb-6">
-        <button className="dark:bg-gray-900 bg-purple-50 dark:hover:bg-gray-800 py-2 px-5 rounded text-lg font-bold shadow-lg hover:shadow-md active:shadow transition-all duration-100 text-gray-900">
+        <button className="dark:bg-gray-900 bg-purple-50 dark:hover:bg-gray-800 py-2 px-5 rounded text-lg font-bold shadow-lg hover:shadow-md active:shadow transition-all duration-100 text-gray-900 dark:text-purple-50">
           i have a passphrase/seed
         </button>
         <button
-          className="dark:bg-gray-900 bg-purple-50 dark:hover:bg-gray-800 py-2 px-5 rounded text-lg font-bold shadow-lg hover:shadow-md active:shadow transition-all duration-100 text-gray-900"
+          className="dark:bg-gray-900 bg-purple-50 dark:hover:bg-gray-800 py-2 px-5 rounded text-lg font-bold shadow-lg hover:shadow-md active:shadow transition-all duration-100 text-gray-900 dark:text-purple-50"
           onClick={() => push('/welcome/new')}
         >
           what&apos;s a passphrase?
