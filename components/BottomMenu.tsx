03dc61879771455db3fcd711e5722096af05bb45
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2021-12-04 11:48:32 +0000

    fix: bigger buttons

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -56,78 +56,81 @@ const BottomMenu: FC<Props> = ({ className }) => {
       )}
     >
       {pathname !== '/dashboard' && (
-        <Link href={isWelcoming ? '#' : '/dashboard'}>
-          <a className="h-12 p-1 bg-purple-400 transition-colors rounded shadow-lg hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default">
+        <Link href="/dashboard">
+          <a
+            className={clsx(
+              'h-12 p-1 bg-purple-400 transition-colors rounded shadow hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default',
+              { 'pointer-events-none': isWelcoming }
+            )}
+          >
             <HomeIcon className="h-full text-purple-50 dark:text-gray-900 transition-colors" />
           </a>
         </Link>
       )}
 
-      <div>
-        <div
-          className={clsx(
-            'flex gap-6 items-end',
-            leftHanded ? 'flex-row-reverse' : 'flex-row'
-          )}
-        >
-          {'share' in navigator ? (
-            <button
-              disabled={isWelcoming}
-              className="w-10 h-16 p-1 bg-purple-400 transition-colors rounded shadow-lg hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default"
-              onClick={onShare}
-            >
-              <ShareIcon className="text-purple-50 dark:text-gray-900 transition-colors" />
-            </button>
-          ) : (
-            <button
-              disabled={isWelcoming || confirmCopyAddress}
-              className={clsx(
-                'p-1 h-16 w-10 rounded shadow-lg transition-colors',
-                confirmCopyAddress
-                  ? 'bg-purple-50'
-                  : 'bg-purple-400 hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default'
-              )}
-              onClick={onCopyAddress}
-            >
-              {confirmCopyAddress ? (
-                <CheckIcon className="text-purple-400" />
-              ) : (
-                <DocumentDuplicateIcon className="text-purple-50 dark:text-gray-900 transition-colors" />
-              )}
-            </button>
-          )}
-
-          <div
-            className={clsx('flex h-16', { 'flex-row-reverse': leftHanded })}
+      <div
+        className={clsx(
+          'flex gap-4 xs:gap-6 items-end',
+          leftHanded ? 'flex-row-reverse' : 'flex-row'
+        )}
+      >
+        {'share' in navigator ? (
+          <button
+            disabled={isWelcoming}
+            className="w-10 h-16 p-1 bg-purple-400 transition-colors rounded shadow hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default"
+            onClick={onShare}
           >
-            <Link href={isWelcoming ? '#' : '/receive/qr'}>
-              <a
-                className={clsx(
-                  'bg-purple-400 transition-colors px-1 h-16 w-10 hover:bg-purple-400 disabled:hover:bg-purple-400 shadow-md disabled:cursor-default',
-                  leftHanded ? 'rounded-r' : 'rounded-l'
-                )}
-              >
-                <LoginIcon
-                  className={
-                    'h-full text-purple-50 dark:text-gray-900 transition-colors w-full -rotate-child-90'
-                  }
-                />
-              </a>
-            </Link>
-            <div className="h-16 p-1 border-t-2 border-b-2 border-purple-400 shadow-lg">
-              <QrcodeIcon className="h-full text-gray-900 dark:text-purple-100 transition-colors" />
-            </div>
-            <Link href={isWelcoming ? '#' : '/send/qrOrAddress'}>
-              <a
-                className={clsx(
-                  'bg-purple-400 transition-colors px-1 h-16 w-10 hover:bg-purple-400 disabled:hover:bg-purple-400 shadow-md disabled:cursor-default',
-                  leftHanded ? 'rounded-l' : 'rounded-r'
-                )}
-              >
-                <PaperAirplaneIcon className="h-full text-purple-50 dark:text-gray-900 transition-colors w-full rotate-[30deg] translate-x-1" />
-              </a>
-            </Link>
+            <ShareIcon className="text-purple-50 dark:text-gray-900 transition-colors" />
+          </button>
+        ) : (
+          <button
+            disabled={isWelcoming || confirmCopyAddress}
+            className={clsx(
+              'p-1 h-16 w-10 rounded shadow transition-colors',
+              confirmCopyAddress
+                ? 'bg-purple-50'
+                : 'bg-purple-400 hover:bg-purple-400 disabled:hover:bg-purple-400 disabled:cursor-default'
+            )}
+            onClick={onCopyAddress}
+          >
+            {confirmCopyAddress ? (
+              <CheckIcon className="text-purple-400" />
+            ) : (
+              <DocumentDuplicateIcon className="text-purple-50 dark:text-gray-900 transition-colors" />
+            )}
+          </button>
+        )}
+
+        <div className={clsx('flex h-16', { 'flex-row-reverse': leftHanded })}>
+          <Link href="/receive/qr">
+            <a
+              className={clsx(
+                'bg-purple-400 transition-colors h-16 px-1 xs:px-2 w-10 xs:w-14 hover:bg-purple-400 disabled:hover:bg-purple-400 shadow disabled:cursor-default',
+                leftHanded ? 'rounded-r' : 'rounded-l',
+                { 'pointer-events-none': isWelcoming }
+              )}
+            >
+              <LoginIcon
+                className={
+                  'h-full text-purple-50 dark:text-gray-900 transition-colors w-full -rotate-child-90'
+                }
+              />
+            </a>
+          </Link>
+          <div className="h-16 p-1 border-t-2 border-b-2 border-purple-400 shadow">
+            <QrcodeIcon className="h-full text-gray-900 dark:text-purple-100 transition-colors" />
           </div>
+          <Link href="/send/qrOrAddress">
+            <a
+              className={clsx(
+                'bg-purple-400 transition-colors h-16 px-1 xs:px-2 w-10 xs:w-14 hover:bg-purple-400 disabled:hover:bg-purple-400 shadow disabled:cursor-default',
+                leftHanded ? 'rounded-l' : 'rounded-r',
+                { 'pointer-events-none': isWelcoming }
+              )}
+            >
+              <PaperAirplaneIcon className="h-full text-purple-50 dark:text-gray-900 transition-colors w-full rotate-[30deg] translate-x-1 -translate-y-0.5" />
+            </a>
+          </Link>
         </div>
       </div>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      screens: {
+        xs: '340px',
+      },
       keyframes: {
         denied: {
           '0%, 100%': { transform: 'translateX(0)' },
